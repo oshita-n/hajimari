fa6174a52e55c59edf2fda86171132ef29f6eebc
oshita-n　　 <oshita-n@whispon.com>
2021-05-05 03:47:03 +0900

    🎨 レイアウトの変更

--- pages/index.js
+++ pages/index.js
@@ -10,12 +10,11 @@ export default function Home() {
       <Header />
       <div className="mt-10 mb-5 text-center">
         <form onSubmit={handleSubmit}>
-          
-          <TextareaAutosize id="text-form" value={message} onChange={handleChange} className="text-gray-500 outline-none	 hover:border-gray-400	 border py-2 px-3 resize-none overflow-hidden w-2/3" maxRows={6} minRows={1}></TextareaAutosize>
+          <TextareaAutosize id="text-form" value={message} onChange={handleChange} className="text-gray-500 outline-none hover:border-gray-400	 border py-2 px-3 resize-none overflow-hidden w-2/3" maxRows={6} minRows={1}></TextareaAutosize>
           <input type="submit" className="ml-2 bg-gray-400 hover:bg-gray-500 text-white font-bold py-2 px-6 rounded relative -top-4 right-0" value="投稿する" />  
         </form>
       </div>
-      <div id="message-area"></div>
+      <div id="message-area" className=""></div>
       <Footer />
   </div>
   )
@@ -28,14 +27,14 @@ const handleChange = (e) => {
 const handleSubmit = (e) => {
   e.preventDefault();
   document.getElementById("text-form").value = "";
-  var p_message = document.createElement("p")
-  p_message.textContent = message
+  var p_message = document.createElement("p");
+  p_message.textContent = message;
   var ma = document.getElementById("message-area");
   p_message.id = "ma_id" + id_num.toString();
   id_num + id_num + 1;
-  p_message.className = "text-xl ml-3 text-gray-400 whitespace-pre-wrap"
-  var hr = document.createElement("hr")
-  hr.className = "border-dotted"
-  ma.after(hr)
-  ma.after(p_message) 
+  p_message.className = "xl:left-52 relative mt-3 text-xl  text-gray-400 whitespace-pre-wrap";
+  var hr = document.createElement("hr");
+  hr.className = "text-center xl:w-4/6 xl:left-52 relative border-dotted";
+  ma.after(hr);
+  ma.after(p_message);
 };
