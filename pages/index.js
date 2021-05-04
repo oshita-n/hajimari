b01cb33175a3bad2b439e77086ca615ea60ab2ca
oshita-n　　 <oshita-n@whispon.com>
2021-05-05 05:20:30 +0900

    🎨 レイアウトの見直し

--- pages/index.js
+++ pages/index.js
@@ -8,13 +8,18 @@ export default function Home() {
   return (
     <div className="container mx-auto">
       <Header />
-      <div className="mt-10 mb-5 text-center">
-        <form onSubmit={handleSubmit}>
-          <TextareaAutosize id="text-form" value={message} onChange={handleChange} className="text-gray-500 outline-none hover:border-gray-400	 border py-2 px-3 resize-none overflow-hidden w-2/3" maxRows={6} minRows={1}></TextareaAutosize>
-          <input type="submit" className="ml-2 bg-gray-400 hover:bg-gray-500 text-white font-bold py-2 px-6 rounded relative -top-4 right-0" value="投稿する" />  
-        </form>
+      <div className="container mx-auto w-2/3">
+        <div className="mt-10 mb-5">
+          <form onSubmit={handleSubmit}>
+            <TextareaAutosize id="text-form" value={message} onChange={handleChange} className="text-gray-500 outline-none hover:border-gray-400 border py-2 px-3 resize-none overflow-hidden w-full" maxRows={6} minRows={1}></TextareaAutosize>
+            <div className="text-right">
+              <input type="submit" className=" ml-2 bg-gray-400 hover:bg-gray-500 text-white font-bold py-2 px-6 rounded" value="投稿する" />  
+            </div>
+          </form>
+        </div>
+        <div id="message-area" className=""></div>
       </div>
-      <div id="message-area" className=""></div>
+      
       <Footer />
   </div>
   )
@@ -32,9 +37,9 @@ const handleSubmit = (e) => {
   var ma = document.getElementById("message-area");
   p_message.id = "ma_id" + id_num.toString();
   id_num + id_num + 1;
-  p_message.className = "hover:text-gray-500 ml-3 xl:left-52 relative mt-3 text-xl  text-gray-400 whitespace-pre-wrap";
+  p_message.className = "hover:text-gray-500 mt-3 text-xl text-gray-400 whitespace-pre-wrap";
   var hr = document.createElement("hr");
-  hr.className = "text-center xl:w-4/6 xl:left-52 relative border-dotted";
+  hr.className = "border-dotted";
   ma.after(hr);
   ma.after(p_message);
 };
