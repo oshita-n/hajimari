f55a132aacb584501db862ff7d7f12dd1e595dfc
oshita-n　　 <oshita-n@whispon.com>
2021-05-05 02:46:00 +0900

    ✨ 文章を追加できるようにした

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,34 @@
-import styles from '../styles/Home.module.css'
 import { Footer } from '../src/components/Footer.js'
 import { Header } from '../src/components/Header.js'
+import TextareaAutosize from 'react-textarea-autosize';
 
+var message;
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="container mx-auto">
       <Header />
-      <div className="flex justify-center mt-10 mb-10">
-        <textarea id="text-form" className="border py-2 px-3  w-6/12 stext-grey-darkest resize-none overflow-hidden" rows="1" onClick={cutLine} onKeyDown={newLine2} onKeyPress={newLine}></textarea>
+      <div className="mt-10 mb-5 text-center">
+        <form onSubmit={handleSubmit}>
+          
+          <TextareaAutosize id="text-form" value={message} onChange={handleChange} className="border py-2 px-3 text-grey-darkest resize-none overflow-hidden w-2/3" maxRows={6} minRows={1}></TextareaAutosize>
+          <input type="submit" className="ml-2 bg-gray-400 hover:bg-gray-500 text-white font-bold py-2 px-6 rounded relative -top-4 right-0" value="投稿する" />  
+        </form>
       </div>
+      <div id="message-area" className="bg-gray-400"></div>
       <Footer />
-    </div>
+  </div>
   )
 }
 
-function newLine(e) {
-  var tf = document.getElementById("text-form");
-  console.log(e.key)
-  if (e.key === "Enter") {
-    tf.rows = tf.rows + 1;
-    console.log(tf.rows);
-  }
-}
+const handleChange = (e) => {
+  message = e.target.value;
+};
 
-function newLine2(e) {
-  var tf = document.getElementById("text-form");
-  console.log(e.key)
-  if (e.key === "Backspace") {
-    if (tf.selectionStart === 1) {
-      tf.rows = tf.rows - 1;
-    }
-  }
-}
-
-function cutLine(e) {
-  var tf = document.getElementById("text-form");
-  console.log(tf.selectionStart);
-}
+const handleSubmit = (e) => {
+  e.preventDefault();
+  document.getElementById("text-form").value = "";
+  var p_message = document.createElement("p")
+  p_message.textContent = message
+  document.getElementById("message-area").after(p_message) 
+};
