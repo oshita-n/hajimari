3368127ce4103c490262c8706d1e0ae2de258c19
oshita-n　　 <oshita-n@whispon.com>
2021-05-05 03:06:57 +0900

    🎨 サイトの説明の追加

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import { Header } from '../src/components/Header.js'
 import TextareaAutosize from 'react-textarea-autosize';
 
 var message;
-
+var id_num = 0;
 export default function Home() {
   return (
     <div className="container mx-auto">
@@ -15,7 +15,7 @@ export default function Home() {
           <input type="submit" className="ml-2 bg-gray-400 hover:bg-gray-500 text-white font-bold py-2 px-6 rounded relative -top-4 right-0" value="投稿する" />  
         </form>
       </div>
-      <div id="message-area" className="bg-gray-400"></div>
+      <div id="message-area"></div>
       <Footer />
   </div>
   )
@@ -30,5 +30,10 @@ const handleSubmit = (e) => {
   document.getElementById("text-form").value = "";
   var p_message = document.createElement("p")
   p_message.textContent = message
-  document.getElementById("message-area").after(p_message) 
+  var ma = document.getElementById("message-area");
+  p_message.id = "ma_id" + id_num.toString();
+  id_num + id_num + 1;
+  p_message.className = ""
+  ma.after(document.createElement("hr"))
+  ma.after(p_message) 
 };
