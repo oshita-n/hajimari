b30f5b05a3d53580657ca7b916475050d7098d0f
oshita-n　　 <oshita-n@whispon.com>
2021-05-11 23:24:35 +0900

    🎨 レイアウトの変更

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,17 @@
 import React from "react"
 import Link from 'next/link';
+import styles from '../../styles/Home.module.css'
 
 export default ({ children }) => (
   <div className="container mx-auto">
-    {children}
-    
+    <div className="mt-10 mb-10">
       <Link href="/">
-        <a><button className="ml-2 bg-gray-400 hover:bg-gray-500 text-white font-bold py-2 px-6 rounded">戻る</button></a>
+        <a><button className="ml-2 bg-gray-400 hover:bg-gray-500 text-white font-bold py-2 px-6 rounded"><div class={ styles.triangle }></div>投稿する</button></a>
       </Link>
+    </div>
+    <div className="container mx-auto bg-pink-200 w-3/6 h-96 rounded py-32">
+      {children}
+    </div>
+    
   </div>
 )
