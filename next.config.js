5f3e0c1b08d54870342153f8f64f688f5aefaef1
oshita-n　　 <oshita-n@whispon.com>
2021-05-11 23:35:19 +0900

    🎨 configを反映

--- next.config.js
+++ next.config.js
@@ -7,5 +7,11 @@ module.exports = {
       }
   
       return config;
-    }
+    },
+    env: {
+        // Reference a variable that was defined in the .env file and make it available at Build Time
+        NEXT_PUBLIC__APYKEY: process.env.NEXT_PUBLIC__APYKEY,
+        NEXT_PUBLIC__DOMAIN: process.env.NEXT_PUBLIC__DOMAIN,
+        NEXT_PUBLIC_ID: process.env.NEXT_PUBLIC_ID,
+      },
   };
