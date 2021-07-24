39a4d7065f55f92ff1f7c2754aa32203b4295cd6
oshita-n　　 <oshita-n@whispon.com>
2021-07-24 09:37:04 +0900

    ボタンを上に表示

--- pages/index.jsx
+++ pages/index.jsx
@@ -16,11 +16,11 @@ export default function Home() {
       <div className="container mx-auto">
         <div className="container mx-auto w-2/3">
           <div className="mt-10 mb-5">
-            <TextareaAutosize id="text-form" placeholder="テキストを入力" onChange={handleChange} className="text-black outline-none py-2 px-3 resize-none overflow-hidden w-full" minRows={1}></TextareaAutosize>
             <div className="text-right">
               <button className="ml-2 bg-gray-400 hover:bg-gray-500 text-white font-bold py-2 px-6 rounded"  value={agendaMessage}>読み上げる</button>
               <button className="ml-2 bg-gray-400 hover:bg-gray-500 text-white font-bold py-2 px-6 rounded">保存</button>
-            </div>            
+            </div> 
+            <TextareaAutosize id="text-form" placeholder="テキストを入力" onChange={handleChange} className="text-black outline-none py-2 px-3 resize-none overflow-hidden w-full" minRows={1}></TextareaAutosize>           
           </div>
         </div>
         <Footer />
